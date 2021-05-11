f8c2e4c179ab06391a246e24a6c61cb81a09a676
Debanu Gupta <debanugupta@gmail.com>
2021-05-11 10:01:10 +0530

    Debugging Redux Applications

--- src/index.js
+++ src/index.js
@@ -8,9 +8,10 @@ const unsubscribe = store.subscribe(() => {
     console.log('Store changed!', store.getState());
   });
 
-store.dispatch (bugAdded("Bug 1"));
-
-store.dispatch (bugResolved(1));
+// store.dispatch (bugAdded("Bug 1"));
+// store.dispatch (bugAdded("Bug 2"));
+// store.dispatch (bugAdded("Bug 3"));
+// store.dispatch (bugResolved(1));
 // store.dispatch (bugRemoved(1));
 
 unsubscribe();
