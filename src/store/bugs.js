6fa944659703e0e15e57d4c1b2b9923b06885cd5
Debanu Gupta <debanugupta@gmail.com>
2021-05-11 20:07:19 +0530

    Designing the Store

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { createSelector } from 'reselect';
 
 let lastId = 0;
 
@@ -6,6 +7,12 @@ const slice = createSlice({
     name: 'bugs',
     initialState: [],
     reducers: {
+
+        bugAssignedToUser: (bugs, action) => {
+            const { bugId, userId} = action.payload;
+            const index = bugs.findIndex(bug => bug.id === bugId);
+            bugs[index].userId = userId;
+        },
 
         bugAdded: (bugs, action) => {
             bugs.push({
@@ -22,5 +29,22 @@ const slice = createSlice({
     }
 });
 
-export const { bugAdded, bugResolved} = slice.actions
+export const { bugAdded, bugResolved, bugAssignedToUser} = slice.actions
 export default slice.reducer;
+
+// Selector
+// export const getUnresolvedBugs = state =>
+//   state.entities.bugs.filter(bug => !bug.resolved);
+
+// Memoization
+
+export const getUnresolvedBugs = createSelector(
+    state => state.entities.bugs,
+    bugs => bugs.filter(bug => !bug.resolved)
+);
+
+export const getBugsByUser = userId =>
+ createSelector(
+    state => state.entities.bugs,
+    bugs => bugs.filter(bug => bug.userId === userId)
+)
