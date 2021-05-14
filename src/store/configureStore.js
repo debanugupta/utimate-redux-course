f7615f1af6847d893d5430c17a94d58b71f2bb12
Debanu Gupta <debanugupta@gmail.com>
2021-05-14 16:12:53 +0530

    Middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import logger from './middleware/logger';
+import toast from './middleware/toast';
 import reducer from "./reducer";
 
 
 export default function () {
-    return configureStore({ reducer });
+    return configureStore({ 
+        reducer,
+        middleware: [
+            ...getDefaultMiddleware(), 
+            logger({destination: "console"}),
+            toast
+        ]
+     });
 }
 
